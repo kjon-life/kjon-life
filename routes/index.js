329bc21a0211b805f1d7e7c8bec5ae0a9c429931
KiloJon <social@kjon.life>
2023-11-06 00:03:34 -0600

    fix: Expanded on articles

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var router = express.Router();
+/* source quotes and maxims from TODO link to github quotes */
+const quotes = require('../quotes.json');
+const maxims = require('../maxims.json');
+/* const articles = require('../articles.json'); */
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -22,19 +26,41 @@ router.get('/', function(req, res, next) {
       content: 'things other people said (that i love)',
       author: 'Latin maxims',
       photographer: 'Zdeněk Macháček',
-      backdropImg: 'images/unsplash/zdenek-machacek-yWSJtyCB2R4-unsplash.jpg',
-      frontImg: 'images/unsplash/zdenek-machacek-yWSJtyCB2R4-unsplash.jpg',
-      photoCreditLink: 'https://unsplash.com/@zmachacek?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText',
-      photoPageLink: 'https://unsplash.com/photos/yWSJtyCB2R4'
+      backdropImg: 'images/unsplash/amariei-mihai-IccZ_e4Tci0-unsplash.jpg',
+      frontImg: 'images/unsplash/amariei-mihai-IccZ_e4Tci0-unsplash.jpg',
+      photoCreditLink: 'https://unsplash.com/@mex_face_poze?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText',
+      photoPageLink: 'https://unsplash.com/photos/IccZ_e4Tci0'
     },
     {
       id: 3,
-      title: 'quotes',
-      content: 'things other people said (that i love)',
-      author: 'Latin maxims',
-      photographer: 'Brett Jordan',
+      title: 'poems',
+      content: '( 雀の涙 : suzume no namida )',
+      author: 'unkown',
+      photographer: 'Amariei Mihai',
       backdropImg: 'images/unsplash/brett-jordan-POMpXtcVYHo-unsplash.jpg',
       frontImg: 'images/unsplash/amariei-mihai-IccZ_e4Tci0-unsplash.jpg',
+      photoCreditLink: 'https://unsplash.com/@mex_face_poze?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText',
+      photoPageLink: 'https://unsplash.com/photos/IccZ_e4Tci0'
+    },
+    {
+      id: 4,
+      title: 'poems',
+      content: 'the perfect day',
+      author: 'from "Many Winters"',
+      photographer: 'Leonardo Sanches',
+      backdropImg: 'images/unsplash/leonardo-sanches-1nCYl1wBhv4-unsplash.jpg',
+      frontImg: 'images/unsplash/amariei-mihai-IccZ_e4Tci0-unsplash.jpg',
+      photoCreditLink: 'https://unsplash.com/ja/@lesanches?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText',
+      photoPageLink: 'https://unsplash.com/photos/1nCYl1wBhv4'
+    },
+    {
+      id: 5,
+      title: 'poems', 
+      content: '( 雀の涙 : suzume no namida )',
+      author: 'XXXXXX',
+      photographer: 'k jon',
+      backdropImg: 'images/curious-pug-enterprise/curious-pug-dark.png',
+      frontImg: 'images/curious-pug-enterprise/curious-pug-dark.png',
       photoCreditLink: 'https://unsplash.com/@mex_face_poze?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText',
       photoPageLink: 'https://unsplash.com/photos/IccZ_e4Tci0'
     }
