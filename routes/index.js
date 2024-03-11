1ff3fdaa64bd49f3f6dc3d991b2320c0510979b9
KiloJon <social@kjon.life>
2024-03-10 21:39:21 -0500

    style: Playing with fontawesome

--- routes/index.js
+++ routes/index.js
@@ -15,60 +15,60 @@ router.get('/', function(req, res, next) {
   // Sample article data
   const articles = [
     {
-      id: 1,
-      title: 'words', 
-      content: '(things i said)',
-      author: 'k jon',
-      photographer: 'Brett Jordan',
-      backdropImg: 'images/unsplash/brett-jordan-POMpXtcVYHo-unsplash.jpg',
-      frontImg: 'images/unsplash/brett-jordan-POMpXtcVYHo-unsplash.jpg',
-      photoCreditLink: 'https://unsplash.com/@brett_jordan?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText',
-      photoPageLink: 'https://unsplash.com/photos/POMpXtcVYHo'
-    },
-    {
-      id: 2,
-      title: 'quotes',
-      content: 'things other people said (that i love)',
-      author: 'Latin maxims',
-      photographer: 'Zdeněk Macháček',
-      backdropImg: 'images/unsplash/zdenek-machacek-yWSJtyCB2R4-unsplash.jpg',
-      frontImg: 'images/unsplash/zdenek-machacek-yWSJtyCB2R4-unsplash.jpg',
-      photoCreditLink: 'https://unsplash.com/@zmachacek?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText',
-      photoPageLink: 'https://unsplash.com/photos/yWSJtyCB2R4'
-    },
-    {
-      id: 3,
-      title: 'poems',
-      content: '( 雀の涙 : suzume no namida )',
-      author: 'unkown',
-      photographer: 'Amariei Mihai',
-      backdropImg: 'images/unsplash/amariei-mihai-IccZ_e4Tci0-unsplash.jpg',
-      frontImg: 'images/unsplash/amariei-mihai-IccZ_e4Tci0-unsplash.jpg',
-      photoCreditLink: 'https://unsplash.com/@mex_face_poze?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText',
-      photoPageLink: 'https://unsplash.com/photos/IccZ_e4Tci0'
-    },
-    {
-      id: 4,
-      title: 'poems',
-      content: 'the perfect day',
-      author: 'from "Many Winters"',
-      photographer: 'Leonardo Sanches',
-      backdropImg: 'images/unsplash/leonardo-sanches-1nCYl1wBhv4-unsplash.jpg',
-      frontImg: 'images/unsplash/leonardo-sanches-1nCYl1wBhv4-unsplash.jpg',
-      photoCreditLink: 'https://unsplash.com/ja/@lesanches?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText',
-      photoPageLink: 'https://unsplash.com/photos/1nCYl1wBhv4'
-    },
-    {
-      id: 5,
-      title: 'poems', 
-      content: '( 雀の涙 : suzume no namida )',
-      author: 'XXXXXX',
-      photographer: 'k jon',
-      backdropImg: 'images/curious-pug-enterprise/curious-pug-dark.png',
-      frontImg: 'images/curious-pug-enterprise/curious-pug-dark.png',
-      photoCreditLink: '',
-      photoPageLink: ''
-    }
+      "id":1,
+      "title":"words",
+      "content":"(things i said)",
+      "author":"k jon",
+      "photographer":"Brett Jordan",
+      "backdropImg":"images/unsplash/brett-jordan-POMpXtcVYHo-unsplash.jpg",
+      "frontImg":"images/unsplash/brett-jordan-POMpXtcVYHo-unsplash.jpg",
+      "photoCreditLink":"https://unsplash.com/@brett_jordan?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+      "photoPageLink":"https://unsplash.com/photos/POMpXtcVYHo"
+   },
+   {
+      "id":2,
+      "title":"quotes",
+      "content":"things other people said (that i love)",
+      "author":"Latin maxims",
+      "photographer":"Zdeněk Macháček",
+      "backdropImg":"images/unsplash/zdenek-machacek-yWSJtyCB2R4-unsplash.jpg",
+      "frontImg":"images/unsplash/zdenek-machacek-yWSJtyCB2R4-unsplash.jpg",
+      "photoCreditLink":"https://unsplash.com/@zmachacek?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+      "photoPageLink":"https://unsplash.com/photos/yWSJtyCB2R4"
+   },
+   {
+      "id":3,
+      "title":"poems",
+      "content":"( 雀の涙 : suzume no namida )",
+      "author":"unkown",
+      "photographer":"Amariei Mihai",
+      "backdropImg":"images/unsplash/amariei-mihai-IccZ_e4Tci0-unsplash.jpg",
+      "frontImg":"images/unsplash/amariei-mihai-IccZ_e4Tci0-unsplash.jpg",
+      "photoCreditLink":"https://unsplash.com/@mex_face_poze?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+      "photoPageLink":"https://unsplash.com/photos/IccZ_e4Tci0"
+   },
+   {
+      "id":4,
+      "title":"poems",
+      "content":"the perfect day",
+      "author":"from \"Many Winters\"",
+      "photographer":"Leonardo Sanches",
+      "backdropImg":"images/unsplash/leonardo-sanches-1nCYl1wBhv4-unsplash.jpg",
+      "frontImg":"images/unsplash/leonardo-sanches-1nCYl1wBhv4-unsplash.jpg",
+      "photoCreditLink":"https://unsplash.com/ja/@lesanches?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+      "photoPageLink":"https://unsplash.com/photos/1nCYl1wBhv4"
+   },
+   {
+      "id":5,
+      "title":"poems",
+      "content":"( 雀の涙 : suzume no namida )",
+      "author":"XXXXXX",
+      "photographer":"k jon",
+      "backdropImg":"images/curious-pug-enterprise/curious-pug-dark.png",
+      "frontImg":"images/curious-pug-enterprise/curious-pug-dark.png",
+      "photoCreditLink":"",
+      "photoPageLink":""
+   }
   ];
 
   const quotesArticle = articles.find(article => article.id === 2);
@@ -98,7 +98,7 @@ router.get('/', function(req, res, next) {
     // Add the refresh button with Font Awesome icon
     quotesArticle.content += `
     <a href="/?page=${currentPage + 1}">
-      <i class="fa fa-refresh fa-4x" aria-hidden="true">Refresh Quotes in i - </i>&nbsp; Refresh Quotes
+      <i class="fa fa-refresh fa-4x" aria-hidden="true"></i>&nbsp; Refresh Quotes
     </a>
   `;
   }
