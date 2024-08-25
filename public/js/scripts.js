def5d2d5947c923b6290be7ed2b9e5773f3fbe10
KiloJon <social@kjon.life>
2024-08-25 12:16:39 -0500

    chore: Clean up js files and references

--- public/js/scripts.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-    var navbar = document.getElementById("navbar");
-    if (navbar.style.display === "block") {
-      navbar.style.display = "none";
-    } else {
-      navbar.style.display = "block";
-    }
-  }
